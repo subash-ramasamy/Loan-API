d7d0a2826b13b1ad78e789f9f6d9d2ae74502006
Subash Ramasamy <subu7505@outlook.com>
2024-12-19 14:03:17 +0530

    error checks

--- loan.py
+++ loan.py
@@ -11,7 +11,7 @@ model = pickle.load(model_file)
 # API endpoints
 @app.route('/')
 def home():
-    return "<h1>Loan Approval Application</h1>"
+    return "<h1>Loan Approval Application updated</h1>"
 
 
 @app.route('/predict', methods=['GET','POST'])
